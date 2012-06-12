3b502e8d0e3fbf28b778ab552858e26eac70ac6a
George Hickman <george@ghickman.co.uk>
2012-06-12 16:02:08 +0100

    Stop an install time error

--- linode/__init__.py
+++ linode/__init__.py
@@ -1,16 +1,3 @@
-__title__ = 'linode'
-__author__ = 'George Hickman'
-__copyright__ = 'Copyright 2012 George Hickman'
-__license__ = 'MIT'
-
-__version__ = (0, 1, 0)
-
-
-# Module namespace.
-
-def get_version():
-    return '.'.join(map(str, __version__))
-
 from .api import Api
 __all__ = ('Api',)
 
--- setup.py
+++ setup.py
@@ -1,23 +1,16 @@
-from os.path import dirname, join
 from setuptools import setup, find_packages
 
-from linode import get_version
-
-
-def fread(fn):
-    with open(join(dirname(__file__), fn), 'r') as f:
-        return f.read()
 
 setup(
-    name = 'linode',
-    version = get_version(),
-    description = 'Python wrapper of the Linode API',
-    long_description = fread('README.rst'),
-    author = 'George Hickman',
-    author_email = 'george@ghickman.co.uk',
-    url = 'http://github.com/ghickman/linode',
-    license = 'MIT',
-    packages = find_packages(),
-    install_requires = ('requests',)
+    name='linode',
+    version='0.2',
+    description='Python wrapper of the Linode API',
+    long_description=open('README.rst').read(),
+    author='George Hickman',
+    author_email='george@ghickman.co.uk',
+    url='http://github.com/ghickman/linode',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=('requests',)
 )
 
