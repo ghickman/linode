16a827ddca1a1adaaf32ef83c4be859c968e4904
George Hickman <george@ghickman.co.uk>
2014-04-21 12:13:03 +0100

    Document params function

--- linode/params.py
+++ linode/params.py
@@ -2,6 +2,12 @@ import requests
 
 
 def get_required_params(api_url):
+    """
+    Build a dictionary of actions with required parameters
+
+    Consume the api.spec data and create a dictionary with actions as keys and
+    their required parameters in a list.
+    """
     url = '{0}/?api_action=api.spec'.format(api_url)
     r = requests.get(url)
     if not r.ok:
