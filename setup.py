390afe8416762f8d1412733397fdc4b8db863a04
George Hickman <george@ghickman.co.uk>
2014-04-21 19:08:55 +0100

    Add trove classifiers

--- setup.py
+++ setup.py
@@ -11,6 +11,18 @@ setup(
     url='http://github.com/ghickman/linode',
     license='MIT',
     packages=find_packages(),
-    install_requires=('requests',)
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
-
